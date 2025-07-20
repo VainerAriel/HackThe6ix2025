a6ac026a8d63ee5495cd55f52b36e311d4056633
VainerAriel <a@vainer.me>
2025-07-20 05:09:23 -0400

    Another cleanup

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon.ico" />
+        <link rel="shortcut icon" href="/icon.ico" />
+        <link rel="apple-touch-icon" href="/icon.ico" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
